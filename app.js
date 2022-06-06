aaf80edf7f31155b11418b49eaa5679c66c2db3b
sahzodbek <sahzodbekrahmonjonov83@gmail.com>
2022-06-06 10:13:09 +0500

    add html and  js new site

---
+++ app.js
@@ -0,0 +1,96 @@
+// let ism = prompt('ismizgizni-kiriting');
+// let nom = ism.charAt().toUppercase();
+// let nom1 = ism.slice(1).toLowerCase();
+// let result = nom + nom1;
+// console.log(result);
+
+// let ism = prompt("Ismingizni Kiriting");
+// let boshidagi = ism.charAt().toUpperCase();
+// let davomi = ism.slice(1).toLowerCase();
+// let result = boshidagi + davomi;
+// console.log(result);
+
+// let yosh = prompt("yoshingizni-kiritin");
+// let oy = yosh * 12;
+// let hafta = yosh * 54;
+// let kun = yosh * 365;
+// let soat = kun * 24;
+// let minut = soat * 60;
+// let secunt = minut * 60;
+// let result1 = `hurmatli ${result} siz ${yosh} yil ${oy} oy ${hafta} hafta ${kun} kun ${soat} soat ${minut} minut ${secunt} sekuntdan beri yashayabsiz yashashni davom eting ☺ ☻`;
+// console.log(result1);
+
+
+
+
+// const name = ['jimSDhrsrh','togaeBVSgm','rREHRHer','eJRTDBSgaer']
+
+// const result = []
+// for(let i = 0; i < name.length; i++){
+// let nom = name[i].charAt().toUpperCase();
+// let nom1 = name[i].slice(1).toLowerCase();
+// let nevname = nom + nom1
+// result.push(nevname + 'bek')
+    
+// }
+// console.log(result)
+
+// let joy = prompt('joy nomini kiriting !');
+// // result = joy.split('');
+// for(let i = joy.length;i = 0; i--){
+//     let nom = joy[i].split('')
+//     consol.log(nom)
+// }
+// <
+
+
+
+
+// let a =  prompt('joy nomini kiriting !');
+
+// let c = a.split(''); 
+// let ism_teskari = ''
+// for(let i = c.length-1; i>= 0; i--){
+//     ism_teskari += c[i]
+// }
+// console.log(ism_teskari)
+
+
+// let son = prompt('biror soni kiriting')
+// for (let i = 0; i < 10; i++) {
+//     result1 = []
+//     result1.push(son * i)
+//     result = `${son}  *  ${i} = ${result1} `
+//     console.log(result)
+// }
+
+
+
+
+
+
+
+
+// let son = prompt('yoshingizni kiriting !')
+// //let armiyayoshi = 18
+// //let chegarasi = 27
+// if(son < 18) {
+//     alert('sizning yoshingiz kichkina')
+// }else if(27 < son) {
+//     alert('sizning yoshingiz katta')
+// }else(18 < son < 27) {
+//     alert("sizning yoshingiz yetarli")
+// }
+// const names = ["ahror" ,"shahzodbek",'nurilloxon','nurjahon','rustamjon','hasanboy']
+
+// for (i = 0; i < names.lenght; i++) {
+//     if(names[i] == 'nurjahon'){
+//         continue
+//     }
+// }
+// console.log(names)
+const a = ['salom, dunyo, qanday', 12, true];
+console.log(a.reverse())
+
+
+
